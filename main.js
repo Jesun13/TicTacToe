d6816f553f50c57a3ddc886c3feff444e89bd8e0
Jesun13 <ihappyimouthi@gmail.com>
2022-07-31 20:21:30 +0300

    TicTacToe
    TicTacToe

---
+++ main.js
@@ -0,0 +1,68 @@
+const area = document.getElementById('area');
+let move = 0;
+let result = '';
+const contentWrapper = document.getElementById('content');
+const modalResult = document.getElementById('modal-result-wrapper');
+const overlay = document.getElementById('overlay');
+const btnClose = document.getElementById('btn-close');
+
+
+
+const check = () => {
+    const boxes = document.getElementsByClassName('box');
+    const valueBoxes = document.getElementsByClassName('box').innerHTML;
+    const arr = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+    
+    for(i = 0; i < arr.length; i++){
+        if(boxes[arr[i][0]].innerHTML == 'X' &&  boxes[arr[i][1]].innerHTML == 'X' && boxes[arr[i][2]].innerHTML == 'X') {
+            result = 'крестики';
+            prepareResult(result);
+        } else if (boxes[arr[i][0]].innerHTML == '0' &&  boxes[arr[i][1]].innerHTML == '0' && boxes[arr[i][2]].innerHTML == '0'){
+            result = 'нолики';
+            prepareResult(result);
+        } 
+        }
+        console.log(result);
+        for(i = 0; i < arr.length; i++){
+            if (move == 9){
+             result = 'ничья';
+             prepareResultDraw(result);
+         }
+        }
+}
+
+area.addEventListener('click', e => {
+    if(e.target.className == 'box'){
+        move % 2 === 0 ? e.target.innerHTML = 'X' : e.target.innerHTML = '0';
+        move++;
+        e.target.classList.add('active');
+        check();
+    }
+});
+
+const prepareResult = winner => {
+    contentWrapper.innerHTML = `Победили ${winner} !`;
+    modalResult.style.display = 'block';
+
+}
+
+const prepareResultDraw = draw => {
+    contentWrapper.innerHTML = `Получилась ${draw} !`;
+    modalResult.style.display = 'block';
+}
+const closeModal = () => {
+    modalResult.style.display = 'none';
+    location.reload();
+}
+
+overlay.addEventListener('click', closeModal);
+btnClose.addEventListener('click', closeModal);
